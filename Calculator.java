1392abf0b1ab002a77d9a4a057214ae6e4021436
MANASACHITI <manasachiti22@gmail.com>
2026-04-09 09:55:13 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -9,5 +9,14 @@ public class Claculator{
      public int multiply(int a ,int b){
       return a*b;
     }
+    public static void main(String[] args){
+    Calculator c=new Calculator();
+      int a=10;
+      int b=9;
+      System.out.println(add(a,b));
+      System.out.println(subtract(a,b));
+    System.out.println(multiply(a,b));
+      System.out.println(divide(a,b));
+      
   }
     
